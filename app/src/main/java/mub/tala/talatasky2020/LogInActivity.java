1d6c60fde8f836fa920be892fadee8b8cd30e824
tala <talamubarki@gmail.com>
2020-10-14 23:15:14 +0300

    Accompany project: task management. I had started with the entering page(Sign up page).

---
+++ app/src/main/java/mub/tala/talatasky2020/LogInActivity.java
@@ -0,0 +1,24 @@
+package mub.tala.talatasky2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText etEmail,etPassword;
+    private Button btnLogin,btnSignup;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassWord);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+    }
+}
