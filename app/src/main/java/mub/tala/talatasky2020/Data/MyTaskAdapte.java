e41b6ae9957012cfe30d3757acb7774a4f6416c2
tala <talamubarki@gmail.com>
2020-11-17 12:50:47 +0200

    Accompany project: task management.

--- app/src/main/java/mub/tala/talatasky2020/Data/MyTaskAdapte.java
+++ app/src/main/java/mub/tala/talatasky2020/Data/MyTaskAdapte.java
@@ -1,14 +1,80 @@
 package mub.tala.talatasky2020.Data;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import mub.tala.talatasky2020.R;
 
 //Adapter
+
+/**
+ *
+ *  target: arrange data source using listview
+ */
+//1.
 public class MyTaskAdapte extends ArrayAdapter<MyTask>
 {
+    /**
+     *constractor
+     * @param context the activity of (app) that this adapter belong to
+     * @param resource XML  design of the item
+     */
+    //fix error
     public MyTaskAdapte(@NonNull Context context, int resource) {
         super(context, resource);
+    }
+
+    /**
+     * bulding single item view
+     * @param position index item in list view
+     * @param convertView item view
+     * @param parent listview
+     * @return
+     */
+
+    @NonNull
+    @Override
+   //3. overriding getview
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+     //3.1
+        View v= LayoutInflater.from(getContext()).inflate(R.layout.item_task_view,parent,false);
+        //3.2 find view by ID
+        TextView tvTitle=v.findViewById(R.id.itmTvTittle);
+        TextView tvImportant=v.findViewById(R.id.itmTvImportant);
+        TextView tvNecessary=v.findViewById(R.id.itemTvNecessery);
+        TextView tvSubject=v.findViewById(R.id.itmTvSubject);
+
+        ImageButton btnDelete=v.findViewById(R.id.itmbtnDelete);
+        ImageButton btnCall=v.findViewById(R.id.itmbtnCall);
+        ImageButton btnEdit=v.findViewById(R.id.itmbtnEdit);
+        //3.3 get the soutable task object
+        MyTask task=getItem(position);
+        //3.4 connect the dot to the view (view the data using item views)
+        tvTitle.setText(task.getTitle());
+        tvSubject.setText(task.getSub());
+        switch(task.getImportant()){
+            case 5: tvNecessary.setBackgroundColor(Color.RED);break;
+            case 4: tvNecessary.setBackgroundColor(Color.YELLOW);break;
+            case 3: tvNecessary.setBackgroundColor(Color.CYAN);break;
+            case 2: tvNecessary.setBackgroundColor(Color.MAGENTA);break;
+            case 1: tvNecessary.setBackgroundColor(Color.RED);break;
+        }
+        switch(task.getNecessary()){
+            case 5: tvImportant.setBackgroundColor(Color.RED);break;
+            case 4: tvImportant.setBackgroundColor(Color.YELLOW);break;
+            case 3: tvImportant.setBackgroundColor(Color.CYAN);break;
+            case 2: tvImportant.setBackgroundColor(Color.MAGENTA);break;
+            case 1: tvImportant.setBackgroundColor(Color.RED);break;
+        }
+        return v;
     }
 }
